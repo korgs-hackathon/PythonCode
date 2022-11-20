11fc7ff1d1d598910822742e53208fb4dd6e3be5
Ant <80172256+TerrifyingAnt@users.noreply.github.com>
2022-11-20 13:15:04 +0300

    detectAndTrack
    Добавлено отслеживание робота, маркеров, а также вычисления угла между направлением движения робота и направления маркера

---
+++ kalman.py
@@ -0,0 +1,139 @@
+import cv2 as cv
+import numpy as np
+import sys
+
+def trackRobot(frame):
+    kernel = np.ones((5,5), np.float32) / 25
+    dst = cv.GaussianBlur(frame,(5,5),0)
+
+    dst = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+    boundaries = [
+        ([30, 125, 126], [179, 255, 255])
+    ]
+
+    for (lower, upper) in boundaries:
+        mask = cv.inRange(dst, np.array(lower, dtype="uint8"), np.array(upper, dtype="uint8"))
+        output = cv.bitwise_and(dst, dst, mask = mask)
+
+    output = cv.cvtColor(output, cv.COLOR_HSV2BGR)
+    ret, thresh = cv.threshold(output, 127, 255, 0)
+
+    edged = cv.Canny(thresh, 30, 200)
+    #5x5 kernel with full of ones
+    kernel = np.ones((3,3),np.uint8)
+    dilate = cv.dilate(edged, kernel, iterations = 1)
+    contours, hierarchy = cv.findContours(dilate, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    cv.drawContours(image=frame, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv.LINE_AA)
+    cv.imshow("xd", frame)
+    for c in contours:
+        if cv.arcLength(c, True) > 10:
+            M = cv.moments(c)
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+        return [int(cX), int(cY)]
+
+
+# Instantiate OCV kalman filter
+class KalmanFilter:
+
+    kf = cv.KalmanFilter(4, 2)
+    kf.measurementMatrix = np.array([[1, 0, 0, 0], [0, 1, 0, 0]], np.float32)
+    kf.transitionMatrix = np.array([[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32)
+
+    def Estimate(self, coordX, coordY):
+        ''' This function estimates the position of the object'''
+        measured = np.array([[np.float32(coordX)], [np.float32(coordY)]])
+        self.kf.correct(measured)
+        predicted = self.kf.predict()
+        return predicted
+
+
+
+#Performs required image processing to get ball coordinated in the video
+class ProcessImage:
+
+    def DetectObject(self):
+
+        vid = cv.VideoCapture(0)
+
+        if(vid.isOpened() == False):
+            print('Cannot open input video')
+            return
+
+        width = int(vid.get(3))
+        height = int(vid.get(4))
+
+        # Create Kalman Filter Object
+        kfObj = KalmanFilter()
+        predictedCoords = np.zeros((2, 1), np.float32)
+
+        while(vid.isOpened()):
+            rc, frame = vid.read()
+
+            if(rc == True):
+                [ballX, ballY] = trackRobot(frame)
+                predictedCoords = kfObj.Estimate(ballX, ballY)
+
+                # Draw Actual coords from segmentation
+                cv.circle(frame, (int(ballX), int(ballY)), 20, [0,0,255], 2, 8)
+                cv.line(frame,(int(ballX), int(ballY + 20)), (int(ballX + 50), int(ballY + 20)), [100,100,255], 2,8)
+                cv.putText(frame, "Actual", (int(ballX + 50), int(ballY + 20)), cv.FONT_HERSHEY_SIMPLEX,0.5, [50,200,250])
+
+                # Draw Kalman Filter Predicted output
+                cv.circle(frame, (int(predictedCoords[0]), int(predictedCoords[1])), 20, [0,255,255], 2, 8)
+                cv.line(frame, (int(predictedCoords[0]) + 16, int(predictedCoords[1]) - 15), (int(predictedCoords[0]) + 50, int(predictedCoords[1]) - 30), [100, 10, 255], 2, 8)
+                cv.putText(frame, "Predicted", (int(predictedCoords[0] + 50), int(predictedCoords[1] - 30)), cv.FONT_HERSHEY_SIMPLEX, 0.5, [50, 200, 250])
+                cv.imshow('Input', frame)
+
+                if (cv.waitKey(300) & 0xFF == ord('q')):
+                    break
+
+            else:
+                break
+
+        vid.release()
+        cv.destroyAllWindows()
+
+    # Segment the green ball in a given frame
+    def DetectBall(self, frame):
+
+        # Set threshold to filter only green color & Filter it
+        lowerBound = np.array([130,30,0], dtype = "uint8")
+        upperBound = np.array([255,255,90], dtype = "uint8")
+        greenMask = cv.inRange(frame, lowerBound, upperBound)
+
+        # Dilate
+        kernel = np.ones((5, 5), np.uint8)
+        greenMaskDilated = cv.dilate(greenMask, kernel)
+        #cv.imshow('Thresholded', greenMaskDilated)
+
+        # Find ball blob as it is the biggest green object in the frame
+        [nLabels, labels, stats, centroids] = cv.connectedComponentsWithStats(greenMaskDilated, 8, cv.CV_32S)
+
+        # First biggest contour is image border always, Remove it
+        stats = np.delete(stats, (0), axis = 0)
+        try:
+            maxBlobIdx_i, maxBlobIdx_j = np.unravel_index(stats.argmax(), stats.shape)
+
+        # This is our ball coords that needs to be tracked
+            ballX = stats[maxBlobIdx_i, 0] + (stats[maxBlobIdx_i, 2]/2)
+            ballY = stats[maxBlobIdx_i, 1] + (stats[maxBlobIdx_i, 3]/2)
+            return [ballX, ballY]
+        except:
+               pass
+
+        return [0,0]
+
+
+#Main Function
+def main():
+
+    processImg = ProcessImage()
+    processImg.DetectObject()
+
+
+if __name__ == "__main__":
+    main()
+
+print('Program Completed!')
